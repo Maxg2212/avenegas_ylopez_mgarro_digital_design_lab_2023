8e0408ee3f9ec7602225d478dbb1b8d9a624847e
Max Garro Mora <maxgarro22@gmail.com>
2023-11-14 00:07:58 -0600

    [Update] Datapath, arm y main

--- datapath.sv
+++ datapath.sv
@@ -19,8 +19,8 @@ module datapath(input logic clk, reset,
   // next PC logic
   mux2entradas #(32) pcmux(PCPlus4, Result, PCSrc, PCNext);
   flopr #(32) pcreg(clk, reset, PCNext, PC);
-  adder #(32) pcadd1(PC, 32'b100, PCPlus4);
-  adder #(32) pcadd2(PCPlus4, 32'b100, PCPlus8);
+  adder_ALU #(32) pcadd1(PC, 32'b100, PCPlus4);
+  adder_ALU #(32) pcadd2(PCPlus4, 32'b100, PCPlus8);
 
   // register file logic
   mux2entradas #(4) ra1mux(Instr[19:16], 4'b1111, RegSrc[0], RA1);
