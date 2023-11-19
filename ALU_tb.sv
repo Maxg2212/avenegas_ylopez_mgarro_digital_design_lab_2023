6328b310a388a38277c6991a98064635375a5841
Yaritza E Lopez B <48070919+yaritzaelena@users.noreply.github.com>
2023-11-18 22:46:10 -0600

    Update ALU_tb.sv

--- ALU_tb.sv
+++ ALU_tb.sv
@@ -1,16 +1,16 @@
-module ALU_tb;
+module ALU_tb();
+
 
   parameter N = 4;
-
   reg [N-1:0] a_i;
   reg [N-1:0] b_i;
   reg [2:0] opcode_i;
 
-  // Salidas
+
   wire [N-1:0] result_o;
   wire [3:0] ALUFlags;
 
-  //Instancia
+  // Instancia 
   ALU #(N) uut (
     .a_i(a_i),
     .b_i(b_i),
@@ -19,29 +19,30 @@ module ALU_tb;
     .ALUFlags(ALUFlags)
   );
 
-  // Inicializar 
   initial begin
-    a_i = 4'b1010; 
-    b_i = 4'b1100; 
-    opcode_i = 3'b010; 
-	$display("a_i: %b, b_i: %b, Resultado: %b, ALUFlags: %b", a_i, b_i, result_o, ALUFlags);
-	 #100 $finish;
+    //AND 
+    a_i = 4'b1100;
+    b_i = 4'b1010;
+    opcode_i = 3'b010;
+    #40; 
 
-    // Pruebas
-    #10 a_i = 4'b1111;
-    #10 b_i = 4'b0000;
-    #10 opcode_i = 3'b011;
+    //OR 
+    a_i = 4'b1100;
+    b_i = 4'b1010;
+    opcode_i = 3'b011;
+    #40;
 
-    #10 b_i = 4'b1100; 
-    #10 opcode_i = 3'b010; 
-	$display("a_i: %b, b_i: %b, Resultado: %b, ALUFlags: %b", a_i, b_i, result_o, ALUFlags);
-    #10 a_i = 4'b1110; 
-    #10 b_i = 4'b1000; 
-    #10 opcode_i = 3'b111; 
-	 
-	 $display("a_i: %b, b_i: %b, Resultado: %b, ALUFlags: %b", a_i, b_i, result_o, ALUFlags);
-	 #100 $finish;
+    //Suma/resta 
+    a_i = 4'b1100;
+    b_i = 4'b1010;
+    opcode_i = 3'b000;
+    #40;
+
+
+    $finish;
   end
+
+endmodule
 
   // Resultados
 endmodule
